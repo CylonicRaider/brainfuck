7ec168fa62a112f4f8027b4bb01abe03e6c1a69f
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-01-23 19:37:53 +0100

    Commence Brainfuck class

--- bf.py
+++ bf.py
@@ -3,6 +3,19 @@
 
 # Brainfuck interpreter.
 
+TAPESIZE = 1000
+
+class Brainfuck:
+    def __init__(self, input, output, tapesize=None):
+        if tapesize is None: tapesize = TAPESIZE
+        self.tapesize = tapesize
+        self.input = input
+        self.output = output
+        self.pointer = 0
+        self.code = None
+        self._cmdp = None
+        self._bytecode = None
+
 def main():
     pass
 
